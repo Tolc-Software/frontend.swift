5d840828f4e9618a9dcf9bc94f3af470b6c0e2c6
srydell <simonwrydell@gmail.com>
2022-05-08 18:42:15 +0200

    Add test on filesystem::path

--- tests/filesystem_paths.cpp
+++ tests/filesystem_paths.cpp
@@ -13,15 +13,18 @@ TEST_CASE("std::filesystem::path gets converted to pathlib.Path",
 #include <filesystem>
 #include <vector>
 
-std::filesystem::path takingPath(std::filesystem::path p) {
+std::filesystem::path
+takingPath(std::filesystem::path const& p) {
 	return p;
 }
 
-std::string toString(std::filesystem::path p) {
-	return p.string();
+std::filesystem::path
+parent(std::filesystem::path const& p) {
+	return p.parent_path();
 }
 
-std::filesystem::path joinPaths(std::vector<std::filesystem::path> arrayToSum) {
+std::filesystem::path
+joinPaths(std::vector<std::filesystem::path> arrayToSum) {
 	std::filesystem::path sum;
 	for (auto f : arrayToSum) {
 		sum /= f;
@@ -30,24 +33,26 @@ std::filesystem::path joinPaths(std::vector<std::filesystem::path> arrayToSum) {
 }
 )";
 
-	auto pythonTestCode = fmt::format(R"(
-# std::filesystem::path translates to pathlib.Path in python
-from pathlib import Path
+	auto objcTestCode = R"(
+// std::filesystem::path corresponds to NSString
+NSString* path = @"Hello/my/name/is/Tolc";
 
-p0 = Path("Hello")
-result0 = {moduleName}.takingPath(p0)
-self.assertEqual(result0, p0)
+// Passing through a function
+NSString* result = [m takingPath:path];
+assert([result isEqualToString:path]);
 
-p1 = Path("Something")
-toString = {moduleName}.toString(p1)
-self.assertEqual(toString, p1.name)
+NSString* parent = [m parent:path];
+assert([parent isEqualToString:@"Hello/my/name/is"]);
 
-result1 = {moduleName}.joinPaths([p0, p1])
-self.assertEqual(result1, p0 / p1)
-)",
-	                                  fmt::arg("moduleName", moduleName));
+NSArray* paths = @[@"to", @"the", @"heart"];
+NSString* joined = [m joinPaths:paths];
+assert([joined isEqualToString:@"to/the/heart"]);
+)";
 
-	auto errorCode = stage.runObjcSwiftTest(cppCode, pythonTestCode);
+	auto swiftTestCode = R"()";
+
+	auto errorCode =
+	    stage.runObjcSwiftTest(cppCode, objcTestCode, swiftTestCode);
 	REQUIRE(errorCode == 0);
 
 	stage.exportAsExample("std::filesystem::path");
